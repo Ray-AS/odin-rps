23bd28cc3676bbca278b50ebe3ef63bce8de2d09
Suthan <abisuthans.business@gmail.com>
2022-12-29 10:25:33 -0500

    Create game function to play 5 rounds, logging winner at end

--- rps.js
+++ rps.js
@@ -17,6 +17,10 @@ game()
 
 */
 
+const NUM_ROUNDS = 5;
+let PLAYER_WINS = 0;
+let COMPUTER_WINS = 0;
+
 function getComputerChoice()
 {
   const choices = ["rock", "paper", "scissors"];
@@ -34,15 +38,52 @@ function playRound(playerSelection, computerSelection)
   {
     playerWin = false;
     tie = true;
+    PLAYER_WINS++;
+    COMPUTER_WINS++;
   }
   else if ((playerSelection === "rock" && computerSelection === "paper") || 
            (playerSelection === "paper" && computerSelection === "scissors") ||
            (playerSelection === "scissors" && computerSelection === "rock"))
   {
     playerWin = false;
+    COMPUTER_WINS++;
+  }
+  else
+  {
+    PLAYER_WINS++;
   }
 
   return tie ? `Tie! Both of you chose ${computerSelection}` : 
          playerWin ? `You win! Your ${playerSelection} beats the computer's ${computerSelection}!` : 
          `You lose! The computer's ${computerSelection} beats your ${playerSelection}!`;
+}
+
+function game()
+{
+  let playerChoice;
+  let computerChoice;
+
+  for(let i = 0; i < NUM_ROUNDS; i++)
+  {
+    playerChoice = prompt("What is your choice? [rock | paper | scissors]").toLowerCase();
+    computerChoice = getComputerChoice();
+
+    console.log(playRound(playerChoice, computerChoice));
+  }
+
+  if(PLAYER_WINS === COMPUTER_WINS)
+  {
+    console.log("You both tie!");
+  }
+  else if (PLAYER_WINS > COMPUTER_WINS)
+  {
+    console.log("You win!");
+  }
+  else
+  {
+    console.log("You lose!");
+  }
+
+  PLAYER_WINS = 0;
+  COMPUTER_WINS = 0;
 }
