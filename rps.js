41087a1194a4d71733fefae9a92aa5e0bb31f0e5
Suthan <abisuthans.business@gmail.com>
2022-12-31 10:28:59 -0500

    Shorten getComputerChoice function

--- rps.js
+++ rps.js
@@ -4,10 +4,8 @@ let COMPUTER_WINS = 0;
 
 function getComputerChoice()
 {
-  const choices = ["rock", "paper", "scissors"];
-  let choice = Math.floor(Math.random() * 3);
-
-  return choices[choice];
+  // Return random choice from array
+  return ["rock", "paper", "scissors"][Math.floor(Math.random() * 3)];
 }
 
 function playRound(playerSelection, computerSelection)
